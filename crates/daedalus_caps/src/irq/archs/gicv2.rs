0bf030f3af0030482fb7ab2f475e04355ea3f01e
stellaurora <jean1398reborn@gmail.com>
2026-08-07 20:29:05 +1000

    feat: impl disable and restore interrupts for GICv2

--- crates/daedalus_caps/src/irq/archs/gicv2.rs
+++ crates/daedalus_caps/src/irq/archs/gicv2.rs
@@ -24,6 +24,32 @@ impl IrqArch for GICv2 {
     fn is_valid_irq(id: u32) -> bool {
         Self::is_valid_irq(id)
     }
+    
+    type InterruptState = InterruptState;
+    
+    unsafe fn disable_interrupts() -> Self::InterruptState {
+        // The DAIF is the state we need to preserve
+        // as we disable interrupts through masking everything
+        let daif: u64;
+
+       unsafe {
+            // Store the current state of the DAIF reg
+            core::arch::asm!("mrs {}, daif", out(reg) daif, options(nomem, nostack));
+
+            // Set bit 1 (IRQ bit) so we mask all IRQ's
+            core::arch::asm!("msr daifset, #2", options(nomem, nostack));
+        }
+
+        // This state will be restored later.
+        InterruptState(daif)
+    }
+    
+    unsafe fn restore_interrupts(state: Self::InterruptState) {
+        unsafe {
+            // Restore the daif state back
+            core::arch::asm!("msr daif, {}", in(reg) state.0, options(nomem, nostack));
+        }
+    }
 }
 
 impl GICv2 {
@@ -31,3 +57,7 @@ impl GICv2 {
         FIRST_DEVICE_INTERRUPT_ID <= id && id < FIRST_SPECIAL_INTERRUPT_ID
     }
 }
+
+/// The interrupt state for a `GICv2` controller. This is the
+/// state of the DAIF which we mask everything to disable interrupts
+struct InterruptState(u64);
